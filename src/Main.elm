076eaf950edb6df459bc44684a95bc8c45c8b498
imtaplet <rockgajoen@gmail.com>
2020-06-15 20:30:10 +0900

    05-http sample

--- src/Main.elm
+++ src/Main.elm
@@ -1,54 +1,54 @@
 module Main exposing (main)
 
 import Browser
-import Html exposing (..)
-import Html.Attributes exposing (..)
-import Html.Events exposing (onInput)
+import Html exposing (Html, text, pre)
+import Http
 
 main =
-  Browser.sandbox { init = init, update = update, view = view }
+  Browser.element 
+    { init = init
+    , update = update
+    , subscriptions = subscriptions 
+    , view = view 
+    }
 
-type alias Model =
-  { name : String
-  , password : String
-  , passwordAgain : String
-  }
+type Model
+  = Failure
+  | Loading
+  | Success String
 
-init : Model
-init =
-  Model "" "" ""
+init : () -> (Model, Cmd Msg)
+init _ =
+  ( Loading
+  , Http.get
+      { url = "https://elm-lang.org/assets/public-opinion.txt"
+      , expect = Http.expectString GotText
+      }
+  )
 
-type Msg 
-  = Name String
-  | Password String
-  | PasswordAgain String
+type Msg
+  = GotText (Result Http.Error String)
 
-update : Msg -> Model -> Model
+update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
   case msg of
-    Name name ->
-      { model | name = name }
-    Password password ->
-      { model | password = password }
-    PasswordAgain password ->
-      { model | passwordAgain = password }
+    GotText result ->
+      case result of
+        Ok fullText ->
+          (Success fullText, Cmd.none)
+        Err _ ->
+          (Failure, Cmd.none)
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+  Sub.none
 
 view : Model -> Html Msg
 view model =
-  div []
-    [ viewInput "text" "Name" model.name Name
-    , viewInput "password" "Password" model.password Password
-    , viewInput "password" "Re-enter Password" model.passwordAgain PasswordAgain
-    , viewValidation model
-    ]
-
-viewInput : String -> String -> String -> (String -> msg) -> Html msg
-viewInput t p v toMsg =
-  input [ type_ t, placeholder p, value v, onInput toMsg ] []
-
-viewValidation : Model -> Html msg
-viewValidation model =
-  if model.password == model.passwordAgain then
-    div [ style "color" "green" ] [ text "OK" ]
-  else
-    div [ style "color" "red" ] [ text "Passdords do not match!" ]
+  case model of
+    Failure ->
+      text "I was unable to load your book."
+    Loading ->
+      text "Loading..."
+    Success fullText ->
+      pre [] [ text fullText ]
