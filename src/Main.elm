3072c415df33347d2916593a503fb306e55b8007
imtaplet <rockgajoen@gmail.com>
2020-06-15 10:09:18 +0900

    Implement a counter sample.

--- src/Main.elm
+++ src/Main.elm
@@ -1,6 +1,24 @@
 module Main exposing (main)
 
-import Html exposing (text)
+import Browser
+import Html exposing (Html, button, div, text)
+import Html.Events exposing (onClick)
 
 main =
-  text "Hello, World!"
+  Browser.sandbox { init = 0, update = update, view = view }
+
+type Msg = Increment | Decrement
+
+update msg model =
+  case msg of
+    Increment ->
+      model + 1
+    Decrement ->
+      model - 1
+
+view model =
+  div []
+    [ button [ onClick Decrement ] [ text "-" ]
+    , div [] [ text (String.fromInt model) ]
+    , button [ onClick Increment ] [ text "+" ]
+    ]
