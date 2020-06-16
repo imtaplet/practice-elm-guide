e0725f4b82b8c1530dfc48b27a144154bea89d5f
imtaplet <rockgajoen@gmail.com>
2020-06-16 23:30:50 +0900

    06-random-svg

--- src/Main.elm
+++ src/Main.elm
@@ -1,6 +1,6 @@
 module Main exposing (main)
 
-import Asset exposing (src, dice)
+import Dice exposing (dice)
 
 import Browser
 import Html exposing (..)
@@ -47,30 +47,9 @@ subscriptions : Model -> Sub Msg
 subscriptions model =
   Sub.none
 
-diceElement x y = 
-  img 
-    [ src dice
-    , style "object-fit" "none"
-    , style "object-position" 
-        ((String.fromInt x ++ "px ") 
-        ++ (String.fromInt y ++ "px"))
-    , width 200
-    , height 200
-    ] []
-
-diceOf n =
-  case n of
-    1 -> diceElement 0 0
-    2 -> diceElement -200 0
-    3 -> diceElement -400 0
-    4 -> diceElement 0 -200
-    5 -> diceElement -200 -200
-    6 -> diceElement -400 -200
-    _ -> div [] [ text "error dice" ]
-
 view : Model -> Html Msg
 view model =
   div []
     [ button [ onClick Roll ] [ text "Roll" ]
-    , diceOf model.diceFace
+    , dice model.diceFace
     ]
