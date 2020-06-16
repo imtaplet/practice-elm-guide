a927554c3affa93e32e28a0973b9ec573ba35b06
imtaplet <rockgajoen@gmail.com>
2020-06-16 18:58:40 +0900

    06-random

--- src/Main.elm
+++ src/Main.elm
@@ -2,10 +2,8 @@ module Main exposing (main)
 
 import Browser
 import Html exposing (..)
-import Html.Attributes exposing (..)
 import Html.Events exposing (..)
-import Http
-import Json.Decode exposing (Decoder, field, string)
+import Random
 
 main =
   Browser.element 
@@ -15,30 +13,32 @@ main =
     , view = view 
     }
 
-type Model
-  = Failure
-  | Loading
-  | Success String
+type alias Model =
+  { dieFace : Int
+  }
+  
 
 init : () -> (Model, Cmd Msg)
 init _ =
-  (Loading, getRandomCatGif)
+  ( Model 1
+  , Cmd.none
+  )
 
 type Msg
-  = MorePlease
-  | GotGif (Result Http.Error String)
+  = Roll
+  | NewFace Int
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
   case msg of
-    MorePlease ->
-      (Loading, getRandomCatGif)
-    GotGif result ->
-      case result of
-        Ok url ->
-          (Success url, Cmd.none)
-        Err _ ->
-          (Failure, Cmd.none)
+    Roll ->
+      ( model
+      , Random.generate NewFace (Random.int 1 6)
+      )
+    NewFace newFace ->
+      ( Model newFace
+      , Cmd.none
+      )
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
@@ -47,35 +47,6 @@ subscriptions model =
 view : Model -> Html Msg
 view model =
   div []
-    [ h2 [] [ text "Random Cats" ]
-    , viewGif model
+    [ h1 [] [ text (String.fromInt model.dieFace) ]
+    , button [ onClick Roll ] [ text "Roll" ]
     ]
-
-viewGif : Model -> Html Msg
-viewGif model =
-  case model of
-    Failure ->
-      div []
-        [ text "I could not load a random cat for some reason."
-        , button [ onClick MorePlease ] [ text "Try Again!" ]
-        ]
-
-    Loading ->
-      text "Loading..."
-    
-    Success url ->
-      div []
-        [ button [ onClick MorePlease, style "display" "block"] [ text "More Please!" ]
-        , img [ src url ] []
-        ]
-
-getRandomCatGif : Cmd Msg
-getRandomCatGif =
-  Http.get
-    { url = "https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=cat"
-    , expect = Http.expectJson GotGif gifDecoder
-    }
-
-gifDecoder : Decoder String
-gifDecoder =
-  field "data" (field "image_url" string)
